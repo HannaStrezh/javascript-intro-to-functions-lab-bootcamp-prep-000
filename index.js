7a75576f65066d9a63ba6f161b1f85be5cca2fb5
Hanna Strezh <hannastrezh@gmail.com>
2018-06-30 00:17:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ function whisper(string) {
   return string.toUpperCase()
 }
 function logShout(string){
-+  console.log(string.toUpperCase());
-+    }
-+function logWhisper(string){
-+    console.log(string.toLowerCase());
-+}
-+function sayHiToGrandma(string){
-+    if (string === string.toLowerCase()){
-+      var lowerCase = string.toLowerCase();
-+      return ('I can\'t hear you!');
-+    }
-+    else if (string === string.toUpperCase()){
-+      var upperCase = 'YES INDEED!';
-+      return ('YES INDEED!');
-+      }
-+      else{
-+        var mixedCase = 'I love you, too.';
-+        return(mixedCase);
-+      }
-+    }
+  console.log(string.toUpperCase());
+   }
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+function sayHiToGrandma(string){
+   if (string === string.toLowerCase()){
+      var lowerCase = string.toLowerCase();
+      return ('I can\'t hear you!');
+   }
+    else if (string === string.toUpperCase()){
+      var upperCase = 'YES INDEED!';
+     return ('YES INDEED!');
+     }
+      else{
+        var mixedCase = 'I love you, too.';
+        return(mixedCase);
+      }
+    }
